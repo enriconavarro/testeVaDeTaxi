2084a2844630a7f588fb7c4fc03d383cf2135ef1
Enrico Navarro <enricolimanavarro@gmail.com>
2019-02-09 19:24:18 -0200

    Ajustes no modelo e na tela da aplicação.

--- frontend/src/components/Inicial.vue
+++ frontend/src/components/Inicial.vue
@@ -1,23 +1,113 @@
     <template>
     <v-container align-content-center fill-height>
-    <v-layout text-xs-center wrap align-center justify-center>
-        <v-flex xs12 sm6>
-        <v-form ref="form" lazy-validation>
-            <v-text-field name="cidade" type="text" label="Cidade" :rules="cidadeRules" v-model="cidade" required>
-            </v-text-field>
+        <v-layout text-xs-center row wrap align-center justify-center v-if="!previsao">
+            <v-flex xs12 sm6>
+            <v-form ref="form" lazy-validation>
+                <v-text-field name="cidade" type="text" label="Cidade" :rules="cidadeRules" v-model="cidade" required />
 
-            <v-btn color="primary" @click="procurar" :loading="loading">
-            Procurar
-            </v-btn>
-        </v-form>
-        </v-flex>
-    </v-layout>
+                <v-btn color="primary" @click="procurar" :loading="loading">
+                Procurar
+                </v-btn>
+            </v-form>
+            </v-flex>
+        </v-layout>
+
+        <v-layout text-xs-center row wrap v-if="previsao" align-center>
+
+            <v-flex xs12>
+
+                <v-layout row wrap align-center justify-center>
+
+                    <v-flex xs12 sm4>
+                        <h1> {{this.cidade}} </h1>
+                    </v-flex>
+
+                </v-layout>
+
+                <v-layout row wrap>
+
+                    <v-flex xs12 sm4>
+                        <span> Mínima </span>
+                        <h3> {{previsao.temperaturaMin + 'º C'}} </h3>
+                    </v-flex>
+
+                    <v-flex xs12 sm4>
+                        <h1> {{previsao.temperatura + 'º C'}} </h1>
+                        <h2> {{previsao.descricao}} </h2>
+                    </v-flex>
+
+                    <v-flex xs12 sm4>
+                        <span> Máxima </span>
+                        <h3> {{previsao.temperaturaMax + 'º C'}} </h3>
+                    </v-flex>
+
+                </v-layout>
+
+                <hr>
+
+                <v-layout row wrap align-center justify-center>
+                    
+                    <v-flex xs12>
+
+                        <v-layout row wrap>
+                            <v-flex xs6 justify-end>
+                                <span> Humidade </span>
+                            </v-flex>
+
+                            <v-flex xs6>
+                                <span> {{previsao.humidade + '%'}} </span>
+                            </v-flex>
+
+                        </v-layout>
+
+                        <v-layout row wrap>
+
+                            <v-flex xs6>
+                                <span> Nascer do sol </span>
+                            </v-flex>
+
+                            <v-flex xs6>
+                                <span> {{previsao.nascerSol}} </span>
+                            </v-flex>
+
+                        </v-layout>
+
+                        <v-layout row wrap>
+
+                            <v-flex xs6>
+                                <span> Pôr do sol </span>
+                            </v-flex>
+
+                            <v-flex xs6>
+                                <span> {{previsao.porSol}} </span>
+                            </v-flex>
+
+                        </v-layout>
+
+                    </v-flex>
+
+                </v-layout>
+
+                <v-layout row wrap>
+
+                    <v-flex xs12>
+                        <v-btn color="default" @click="voltar">
+                            Voltar
+                        </v-btn>
+                    </v-flex>
+
+                </v-layout>
+            </v-flex>
+
+        </v-layout>
+
     </v-container>
     </template>
 
 <script>
 
 import ServiceUtil from '@/services/serviceUtil';
+import PrevisaoModel from '@/models/previsao.js';
 
 export default {
     data: () => ({
@@ -26,7 +116,8 @@ export default {
         v => !!v || 'É obrigatório informar uma cidade'
         ],
         serviceUtil: new ServiceUtil(),
-        loading: false
+        loading: false,
+        previsao: undefined
     }),
 
     methods: {
@@ -43,8 +134,7 @@ export default {
 
                     if (response.cod == 200) {
 
-                        //TODO Tratar dados
-                        console.log(response)
+                        this.previsao = new PrevisaoModel(response)
                     
                     } else {
 
@@ -57,6 +147,12 @@ export default {
                     }
                 })
             }
+        },
+
+        voltar() {
+
+            this.cidade = undefined
+            this.previsao = undefined
         }
     }
 }
--- frontend/src/models/previsao.js
+++ frontend/src/models/previsao.js
@@ -8,12 +8,30 @@ export default class PrevisaoModel {
             previsao = {}
         }
 
-        this.descricao = previsao.wheater[0].description
+        this.descricao = this.tratarDescricao(previsao.weather[0].description)
+        this.data = new Date(previsao.dt)
         this.temperatura = previsao.main.temp
         this.temperaturaMax = previsao.main.temp_max
         this.temperaturaMin = previsao.main.temp_min
         this.humidade = previsao.main.humidity
-        this.nascerSol = previsao.sys.sunrise
-        this.porSol = previsao.sys.sunset
+        this.nascerSol = this.conversaoUnixToHoras(previsao.sys.sunrise)
+        this.porSol = this.conversaoUnixToHoras(previsao.sys.sunset)
+    }
+
+    conversaoUnixToHoras = function (t) {
+
+        var dt = new Date(t*1000);
+        var hr = dt.getHours();
+        var m = "0" + dt.getMinutes();
+        var s = "0" + dt.getSeconds();
+        return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
+    }
+
+    tratarDescricao = function(texto) {
+
+        return texto.toLowerCase()
+            .split(' ')
+            .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(' ')
     }
 }
